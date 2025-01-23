376d425f0a11b46d321cecf5e4580f56d6dcae1b
Beastyrabbit <16068109+beastyrabbit@users.noreply.github.com>
2025-01-23 22:51:50 +0100

    Ensure consistent formatting and add TTS playback handling
    Refactored code for consistent formatting across methods, improving readability and maintainability. Added TTS command functionality to play audio via PyAudio, including MP3 decoding with pydub and playback handling. Established better error handling for OBS client connection.

--- src/helperfunc/global_methods.py
+++ src/helperfunc/global_methods.py
@@ -1,3 +1,4 @@
+import os
 import random
 from datetime import datetime
 
@@ -13,166 +14,188 @@ global_settings_Manager: SettingsManager = SettingsManager()
 
 
 def inc_global_chat_counter():
-	redis_client.hincrby('stream:global', 'messege_counter', 1)
+    redis_client.hincrby("stream:global", "messege_counter", 1)
 
 
 def calculate_interest(user: User):
-	# check if user has invested before
-	if user.points_invested == 0:
-		return 0
-	# get the amount of points invested
-	invested = int(user.points_invested)
-	# get the timestamp
-	timestamp = user.timestamp_investment
-	# rate is 0.02
-	daily_interest_rate = global_settings_Manager.getCommand(OfficialCommands.invest).daily_interest_rate
-	# calcautlte the days since investment in days
-	current_time = datetime.utcnow()
-	timestamp = datetime.fromisoformat(timestamp)
-	days_since_investment = (current_time - timestamp).days
-	if days_since_investment == 0:
-		return 0
+    # check if user has invested before
+    if user.points_invested == 0:
+        return 0
+    # get the amount of points invested
+    invested = int(user.points_invested)
+    # get the timestamp
+    timestamp = user.timestamp_investment
+    # rate is 0.02
+    daily_interest_rate = global_settings_Manager.getCommand(
+        OfficialCommands.invest
+    ).daily_interest_rate
+    # calcautlte the days since investment in days
+    current_time = datetime.utcnow()
+    timestamp = datetime.fromisoformat(timestamp)
+    days_since_investment = (current_time - timestamp).days
+    if days_since_investment == 0:
+        return 0
 
-	# Calculate compound interest daily
-	total_amount = invested * (1 + daily_interest_rate) ** days_since_investment
-	# round down to 0 decimal points
-	total_amount = int(total_amount)
-	interest = total_amount - invested
-	# store the add collected interest in the database for the user
-	# check if user has collected interest before
-	user.points_collected_from_investment += interest
-	user.timestamp_investment = current_time.isoformat()
+    # Calculate compound interest daily
+    total_amount = invested * (1 + daily_interest_rate) ** days_since_investment
+    # round down to 0 decimal points
+    total_amount = int(total_amount)
+    interest = total_amount - invested
+    # store the add collected interest in the database for the user
+    # check if user has collected interest before
+    user.points_collected_from_investment += interest
+    user.timestamp_investment = current_time.isoformat()
 
-	# Return total balance including interest
-	return interest
+    # Return total balance including interest
+    return interest
 
 
 def get_discussion_topic_for_technology():
-	# Get the last 10 discussion topics
-	last_topics = redis_client.lrange('discussion_topics', -10, -1)
-	last_topics = [topic for topic in last_topics]
-	# Generate a new topic
-	completion = client.chat.completions.create(
-		model='gpt-4o-mini',
-		messages=[
-			{
-				'role': 'user',
-				'content': f"Generate a unique discussion topic about technology. Don't use highly philosophical question, a bit is ok. It should only be one Sentence/Question Avoid these topics: {', '.join(last_topics)}",
-			}
-		],
-	)
-	new_topic = completion.choices[0].message.content.strip()
+    # Get the last 10 discussion topics
+    last_topics = redis_client.lrange("discussion_topics", -10, -1)
+    last_topics = [topic for topic in last_topics]
+    # Generate a new topic
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": f"Generate a unique discussion topic about technology. Don't use highly philosophical question, a bit is ok. It should only be one Sentence/Question Avoid these topics: {', '.join(last_topics)}",
+            }
+        ],
+    )
+    new_topic = completion.choices[0].message.content.strip()
 
-	# Add the new topic to Redis
-	redis_client.rpush('discussion_topics', new_topic)
+    # Add the new topic to Redis
+    redis_client.rpush("discussion_topics", new_topic)
 
-	# Print the new unique topic
-	return new_topic
+    # Print the new unique topic
+    return new_topic
 
 
 def let_ai_narrate_the_fight(fight_sequence):
-	# Convert the fight sequence into a readable log
-	formatted_fight = '\n'.join(fight_sequence)
+    # Convert the fight sequence into a readable log
+    formatted_fight = "\n".join(fight_sequence)
 
-	# Call the AI model to narrate the fight
-	completion = client.chat.completions.create(
-		model='gpt-4o-mini',
-		messages=[
-			{
-				'role': 'user',
-				'content': (
-					'The Narrator should do a very short battle report. like max 3 short sentences' 'Include dramatic flair, intense emotions, and heroic descriptions. ' "Here's the fight log:\n\n" f'{formatted_fight}'),
-			}
-		],
-		max_tokens=300,
-		temperature=0.7,
-	)
+    # Call the AI model to narrate the fight
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": (
+                    "The Narrator should do a very short battle report. like max 3 short sentences"
+                    "Include dramatic flair, intense emotions, and heroic descriptions. "
+                    "Here's the fight log:\n\n"
+                    f"{formatted_fight}"
+                ),
+            }
+        ],
+        max_tokens=300,
+        temperature=0.7,
+    )
 
-	return completion.choices[0].message.content.strip()
+    return completion.choices[0].message.content.strip()
 
 
-def get_text_to_spech(text: str, output_name: str = 'output', voice: str = 'onyx', hd: bool = False) -> bytes:
-	# Text-to-Speech
-	if hd:
-		modeltype = 'tts-1-hd'
-	else:
-		modeltype = 'tts-1'
+def get_text_to_spech(text: str, voice: str = "onyx", hd: bool = False) -> bytes:
+    # Text-to-Speech
+    if hd:
+        modeltype = "tts-1-hd"
+    else:
+        modeltype = "tts-1"
 
-	response = client.audio.speech.create(
-		model=modeltype,
-		voice=voice,  # Choose from available voices: alloy, echo, fable, onyx, shimmer
-		input=text,
-	)
-	# Return the audio as bytes for playback
-	audio_data = response.stream_bytes()
-	return audio_data
+    response = client.audio.speech.create(
+        model=modeltype,
+        voice=voice,  # Choose from available voices: alloy, echo, fable, onyx, shimmer
+        input=text,
+    )
+    # Return the audio as bytes for playback
+    print(response)
+    # Save the audio output into folder AUDIO_PATH
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    AUDIO_PATH = os.path.join(BASE_DIR, "sound", "test_audio.mp3")
+    # check file exists and delete it
+    if os.path.exists(AUDIO_PATH):
+        os.remove(AUDIO_PATH)
+    response.stream_to_file(AUDIO_PATH)
+    audio_data = response.content
+    return audio_data
 
 
 def translate_text(text):
-	completion = client.chat.completions.create(
-		model='gpt-4o-mini',
-		messages=[
-			{
-				'role': 'user',
-				'content': f"Figure out the langauge for this input: '{text}' if its english translate it to german and if its german translate it to english."
-				           ' Your answer should be very short. If its a sentence translate the sentence. If the translation is not 1 to 1 add a super extra soft explanation.',
-			}
-		],
-	)
-	return completion.choices[0].message.content.strip()
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": f"Figure out the langauge for this input: '{text}' if its english translate it to german and if its german translate it to english."
+                           " Your answer should be very short. If its a sentence translate the sentence. If the translation is not 1 to 1 add a super extra soft explanation.",
+            }
+        ],
+    )
+    return completion.choices[0].message.content.strip()
 
-	# Ensure store_value is an integer
-	store_value = int(store_value)
+    # Ensure store_value is an integer
+    store_value = int(store_value)
 
-	# Check if the user already exists in the Redis hash
-	if not redis_client.hexists(redis_key, username):
-		# Initialize the user's value to 0 if not present
-		redis_client.hset(redis_key, username, 0)
+    # Check if the user already exists in the Redis hash
+    if not redis_client.hexists(redis_key, username):
+        # Initialize the user's value to 0 if not present
+        redis_client.hset(redis_key, username, 0)
 
-	# Increment the value for the username
-	redis_client.hincrby(redis_key, username, store_value)
+    # Increment the value for the username
+    redis_client.hincrby(redis_key, username, store_value)
 
-	# Increment the total count for the command
-	redis_client.hincrby(count_key, username, 1)
+    # Increment the total count for the command
+    redis_client.hincrby(count_key, username, 1)
 
-	# Update the timestamp in the separate hash
-	current_time = datetime.utcnow().isoformat()
-	redis_client.hset(timestamp_key, username, current_time)
-	logger.debug(
-		f'Updated Redis -> Chat command: {chat_command}, Username: {username}, Incremented by: {store_value}, Time: {current_time}')
+    # Update the timestamp in the separate hash
+    current_time = datetime.utcnow().isoformat()
+    redis_client.hset(timestamp_key, username, current_time)
+    logger.debug(
+        f"Updated Redis -> Chat command: {chat_command}, Username: {username}, Incremented by: {store_value}, Time: {current_time}"
+    )
 
 
 def do_the_cleaning_command(user: User) -> tuple[int, bool]:
-	# Get the random value
-	if global_settings_Manager.check_user_not_in_timeout(user, OfficialCommands.roomba):
-		max_value = global_settings_Manager.getCommand(OfficialCommands.roomba).current_max_value
-		get_random = random.randint(1, max_value)
-		_logger.info(
-			f'{user.username} cleaned up {get_random} from a max of {global_settings_Manager.getCommand(OfficialCommands.roomba).current_max_value}')
-		# we will store the person who hit the max value
-		if get_random == max_value:
-			# check if user has max value hit before
-			user.clean_max_hit_counter += 1
-			global_settings_Manager.getCommand(OfficialCommands.roomba).current_max_value *= 10
-		# Store the cleaned value in a database
-		user.clean_points_collected += get_random
-		# Store the amount of messages cleaned up in a database
-		return get_random, get_random == max_value
-	return 0, False
+    # Get the random value
+    if global_settings_Manager.check_user_not_in_timeout(user, OfficialCommands.roomba):
+        max_value = global_settings_Manager.getCommand(
+            OfficialCommands.roomba
+        ).current_max_value
+        get_random = random.randint(1, max_value)
+        _logger.info(
+            f"{user.username} cleaned up {get_random} from a max of {global_settings_Manager.getCommand(OfficialCommands.roomba).current_max_value}"
+        )
+        # we will store the person who hit the max value
+        if get_random == max_value:
+            # check if user has max value hit before
+            user.clean_max_hit_counter += 1
+            global_settings_Manager.getCommand(
+                OfficialCommands.roomba
+            ).current_max_value *= 10
+        # Store the cleaned value in a database
+        user.clean_points_collected += get_random
+        # Store the amount of messages cleaned up in a database
+        return get_random, get_random == max_value
+    return 0, False
 
 
 def generate_rnd_amount_to_steal() -> int:
-	# Pre-configured parameters
-	max_value = global_settings_Manager.getCommand(OfficialCommands.roomba).current_max_value
-	luck_factor = 20.0  # Higher values make high numbers less likely
+    # Pre-configured parameters
+    max_value = global_settings_Manager.getCommand(
+        OfficialCommands.roomba
+    ).current_max_value
+    luck_factor = 20.0  # Higher values make high numbers less likely
 
-	# Create a range of possible values
-	values = np.linspace(0, max_value, 1000)
+    # Create a range of possible values
+    values = np.linspace(0, max_value, 1000)
 
-	# Define weights: Exponentially decrease probability for higher values
-	weights = np.exp(-luck_factor * (values / max_value))
-	weights /= weights.sum()  # Normalize to sum to 1
+    # Define weights: Exponentially decrease probability for higher values
+    weights = np.exp(-luck_factor * (values / max_value))
+    weights /= weights.sum()  # Normalize to sum to 1
 
-	# Generate a single result
-	result = np.random.choice(values, size=1, p=weights)
-	return int(result[0])
+    # Generate a single result
+    result = np.random.choice(values, size=1, p=weights)
+    return int(result[0])
